4fca0996953e955171266502133cdb5d2fdbba43
Ivy Zhang <ivyzhang1017@hotmail.com>
2021-03-03 17:37:42 -0700

    Add roxygen2 and document the descriptions

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,18 @@
+#' To bind two factors
+#'
+#' Create a new factor which combine two input factors.
+#' The levels of the new factor is the union of those of the input factors.
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
+#' a <- factor(c("character", "hits", "your", "eyeballs"))
+#' b <- factor(c("but", "integer", "where it", "counts"))
+#' factor(c(as.character(a), as.character(b)))
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
